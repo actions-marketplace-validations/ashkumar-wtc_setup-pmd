6c5b1227204076040d3e19aa027d12b8d845a646
ashkumar-wtc <ashish.kumar-2@wundermanthompson.com>
2020-09-11 16:45:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function installPMD(){
   var download = 'wget https://github.com/pmd/pmd/releases/download/pmd_releases%2F6.27.0/pmd-bin-6.27.0.zip -P /tmp'
   var unzip = 'unzip /tmp/pmd-bin-6.27.0.zip -d /tmp'
   var mk = 'mkdir -p $HOME/pmd'
-  var mbin = 'mkdir -p $HOME/bin/pmd'
+  var mbin = 'mkdir -p $HOME/bin'
   var mv = 'mv /tmp/pmd-bin-6.27.0/* $HOME/pmd'
   exec(download+' && '+unzip+' && '+mk+' && '+mv+' && '+mbin , function(error, stdout, stderr){
     if(error) core.setFailed(stderr)
